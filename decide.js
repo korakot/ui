a96db6aa495ecf7966d4f5aee03dbdfd4db9c775
Korakot Chaovavanich <korakot@gmail.com>
2026-09-13 17:14:23 +0700

    decide.js: note placeholder → ความเห็น / Comment

--- decide.js
+++ decide.js
@@ -39,7 +39,7 @@
   var STR = {
     en: {
       overall: 'Overall comment',
-      notePh: 'Note / resolution',
+      notePh: 'Comment',
       hint: 'unclicked = skipped \u2014 just add a note, no click needed',
       submit: 'Submit \u2197',
       sent: 'Sent',
@@ -47,7 +47,7 @@
     },
     th: {
       overall: '\u0e04\u0e27\u0e32\u0e21\u0e40\u0e2b\u0e47\u0e19\u0e42\u0e14\u0e22\u0e23\u0e27\u0e21',
-      notePh: '\u0e42\u0e19\u0e49\u0e15/\u0e17\u0e32\u0e07\u0e41\u0e01\u0e49',
+      notePh: '\u0e04\u0e27\u0e32\u0e21\u0e40\u0e2b\u0e47\u0e19',
       hint: '\u0e44\u0e21\u0e48\u0e01\u0e14 = \u0e02\u0e49\u0e32\u0e21 \u2014 \u0e43\u0e2a\u0e48\u0e41\u0e04\u0e48\u0e42\u0e19\u0e49\u0e15 \u0e44\u0e21\u0e48\u0e15\u0e49\u0e2d\u0e07\u0e01\u0e14\u0e40\u0e25\u0e37\u0e2d\u0e01 \u0e01\u0e47\u0e44\u0e14\u0e49',
       submit: '\u0e2a\u0e48\u0e07 \u2197',
       sent: '\u0e2a\u0e48\u0e07\u0e41\u0e25\u0e49\u0e27',
